ad0649be0fa76ac776b76ec7c3c796379cfc6385
admin <admin@DESKTOP-8L2IG92>
2021-04-09 06:46:18 +0900

    ex3no2

---
+++ ex3no2/src/ex3no2/ex3no2.java
@@ -0,0 +1,18 @@
+package ex3no2;
+
+public class ex3no2 {  // 94p
+   public static void main(String[] args) {
+	  int i=5,j=0;
+	  
+	  j = i++;
+	  System.out.println("j = i++; 실행 후, i = "+i+", j = "+j); 
+	   
+	  i = 5;
+	  j = 0;
+	  
+	  j= ++i;
+	  System.out.println("j = ++i; 실행 후, i = "+i+", j = "+j);
+	   
+   }
+
+}
